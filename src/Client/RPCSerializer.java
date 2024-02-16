a2658b3bfc0617caab35b358776af898d78c03fb
anwithayi <anyiandwinnie@gmail.com>
2024-02-16 03:15:32 -0800

    presuocode

--- src/Client/RPCSerializer.java
+++
@@ -1,5 +0,0 @@
-package Client;
-//Handles the serialization and deserialization of RPC requests and responses to facilitate communication with the server.
-public class RPCSerializer {
-    
-}
--- src/Server/clientHandler.java
+++
@@ -1,5 +0,0 @@
-package Server;
-//Responsible for managing the communication with an individual client. It runs in a separate thread for each client and handles all RPCs
-public class clientHandler {
-
-}
